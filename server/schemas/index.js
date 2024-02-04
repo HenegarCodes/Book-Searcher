7479c44d9f9ba9882ba36e534583223ed34ec88f
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2024-02-04 13:34:43 -0700

    adding tpyedefs, and resolvers

---
+++ server/schemas/index.js
@@ -0,0 +1,7 @@
+const gql = require
+apol
+
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+module.exports = { typeDefs, resolvers };
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,62 @@
+const { User } = require('../models');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+        if (context.user){
+            return User.findOne({_id:context.user._id});
+        }
+    }
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    async saveBook( parent, args, context) {
+        console.log(context.user);
+        try {
+          const updatedUser = await User.findOneAndUpdate(
+            { _id: context.user._id },
+            { $addToSet: { savedBooks: args } },
+            { new: true, runValidators: true }
+          );
+          return (updatedUser);
+        } catch (err) {
+          console.log(err);
+          throw AuthenticationError;
+        }
+      },
+      // remove a book from `savedBooks`
+      async deleteBook(parents, args, context) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId: args.bookId } } },
+          { new: true }
+        );
+        if (!updatedUser) {
+            throw AuthenticationError;        }
+        return (updatedUser);
+      },
+}
+}
+
+module.exports = resolvers;
