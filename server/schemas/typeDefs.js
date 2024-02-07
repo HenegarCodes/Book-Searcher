89d2c55c97dd5d088e7e19ecbbf1b22bb8c79e66
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2024-02-06 20:54:14 -0700

    deploying

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,38 +1,47 @@
-const typeDefs = `
-  type User {
-    _id: ID!
-    username: String!
-    email: String!
-    bookCount: Int!
-    savedBooks: [Book]!
+const { gql } = require('apollo-server-express');
 
-    
-  }
-
-  type Book {
-    bookId: ID!
-    authors: [String]!
-    description: String!
-    title: String!
-    image: String!
-    link: String!
-  }
-
-  type Auth {
-    token: String!
-    user: User!
-  }
-
-  type Query {
-    me: User!
-  }
-
-  type Mutation {
-login(email: String!, password: String!):Auth   
-addUser(username: String!, email: String!, password: String!):Auth
-saveBook(authors: [String]!, description: String!, title: String!, bookId: ID!, image: String!, link: String!):User
-deleteBook(bookId: ID!):User
+// typeDefs
+const typeDefs = gql`
+    type Book {
+        _id: ID
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        forSale: String
+        link: String
+        title: String
+    }
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]        
+    }
+    type Query {
+        me: User
+    }
+    type Auth {
+    token: ID!
+    user: User
+    }
+    input SavedBookInput {
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        forSale: String
+        link: String
+        title: String
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(book: SavedBookInput): User
+        removeBook(bookId: String!): User
 }
 `;
+
 
 module.exports = typeDefs;
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,38 +1,34 @@
 const jwt = require('jsonwebtoken');
-
-
-const secret = 'secret';
+const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  
+  //  for  authenticated routes
   authMiddleware: function ({ req }) {
-    // allows token to be sent via  req.query or headers
-    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    let token = req.query.token || req.headers.authorization || req.body.token;
 
 
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
-    // if no token , return req
+
     if (!token) {
       return req;
     }
-
-
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
+      return res.status(400).json({ message: 'invalid token!' });
     }
-
 
     return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
-    
+
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
