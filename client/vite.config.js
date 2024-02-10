654da4e5c87116382b6c877a733a44288ff12a4f
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2024-02-09 18:32:44 -0700

    adding complete code with tutor

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,15 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3002,
+    server: {
+    port: 3000,
     open: true,
     proxy: {
-      '/api': {
-        target: 'http://localhost:3002',
+      '/graphql': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
         secure: false,
-        changeOrigin: true
-      }
+      },
     }
   }
 })
